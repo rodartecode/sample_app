83e98792db97d95b69cb1f02de5ff0b5ac3aa643
Jesse <jrodarte@mail.ccsf.edu>
2019-12-29 18:20:25 -0800

    full title helper

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,3 @@
 module StaticPagesHelper
+  #test
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,20 +1,20 @@
-require 'test_helper'
+require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
-def setup
-  @base_title = "Ruby on Rails Tutorial Sample App"
-end
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   test "should get root" do
     get root_url
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
-  
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
