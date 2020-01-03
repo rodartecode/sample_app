8fc6e1ea0e1dd17566ecefaa8c041d98ebeec424
Jesse <jrodarte@mail.ccsf.edu>
2020-01-03 15:49:47 -0800

    Implemented authorization for profile changes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -33,7 +33,20 @@ module SessionsHelper
     end
   end
 
+  def current_user?(user)
+    user&.== current_user
+  end
+
   def logged_in?
     !current_user.nil?
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -10,6 +10,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template "users/edit"
     patch user_path(@user), params: { user: { name: "",
@@ -19,17 +20,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template "users/edit"
   end
 
-  test "successful edit" do
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template "users/edit"
+    assert_not_nil session[:forwarding_url]
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name: name,
                                               email: email,
-                                              password: "",
-                                              password_confirmation: "" } }
+                                              password: "password",
+                                              password_confirmation: "password" } }
     assert_not flash.empty?
     assert_redirected_to @user
+    assert_nil session[:forwarding_url]
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
